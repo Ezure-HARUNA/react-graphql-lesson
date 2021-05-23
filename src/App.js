de2ee3f817856f68ee6355ea04258abb01fa9ad3
Ezure-HARUNA <haruna.e@80and.co>
2021-05-23 20:19:45 +0900

    Reactプロジェクト作成

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import './App.module.css';
-import { ApolloProvider } from "@apollo/react-hooks"
-import { ApolloClient } from "@apollo/client"
-import { Route, BrowserRouter, Switch } from "react-router-dom"
-import Auth from "./components/Auth"
-import MainPage from "./components/MainPage"
-
+import React from "react";
+import styles from "./App.module.css";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Auth from "./components/Auth";
+import MainPage from "./components/MainPage";
+// import StateContextProvider from "./context/StateContext";
 const client = new ApolloClient({
   uri: "http://127.0.0.1:8000/graphql/",
   headers: {
@@ -14,14 +14,21 @@ const client = new ApolloClient({
       : "",
   },
   cache: new InMemoryCache(),
-})
-
-const App = () => {
+});
+function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <ApolloProvider client={client}>
+      {/* <StateContextProvider> */}
+        <div className={styles.app}>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Auth} />
+              <Route exact path="/employees" component={MainPage} />
+            </Switch>
+          </BrowserRouter>
+        </div>
+      {/* </StateContextProvider> */}
+    </ApolloProvider>
   );
 }
-
 export default App;
